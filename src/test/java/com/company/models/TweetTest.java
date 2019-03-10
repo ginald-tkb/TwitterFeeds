f99e0fa589926d4be99b3144ce744cd6f0b447dc
Tshiamo <tshiamo.tkb@gmail.com>
2019-03-10 18:44:04 +0200

    Create model test classes

---
+++ src/test/java/com/company/models/TweetTest.java
@@ -0,0 +1,14 @@
+package com.company.models;
+
+import org.junit.Test;
+
+/**
+ * Created by tshiamo on 2019/03/10.
+ */
+public class TweetTest {
+    @Test
+    public void setAuthorTest() throws Exception {
+        //TODO: Set Author adds a tweet to the passed User object
+    }
+
+}
---
+++ src/test/java/com/company/models/UserTest.java
@@ -0,0 +1,29 @@
+package com.company.models;
+
+import org.junit.Test;
+
+/**
+ * Created by tshiamo on 2019/03/10.
+ */
+public class UserTest {
+    @Test
+    public void UserConstructorSetDefaultTest() throws Exception {
+        //TODO: implementation
+    }
+
+    @Test
+    public void addTweetTest() throws Exception {
+        //TODO: Is tweet added to the list
+    }
+
+    @Test
+    public void addFollowerTest() throws Exception {
+        //TODO: Is Follower added to the list
+    }
+
+    @Test
+    public void compareTo() throws Exception {
+        //TODO: Does the sort work
+    }
+
+}
